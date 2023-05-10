590ee19ba1f8b360e9bc2ae4fe0c66837d039735
manueldvr <manueldvr@yahoo.com>
2023-05-10 13:40:03 -0300

    Changes to the pom: dependency Changes to web.model.Beer

---
+++ src/main/java/com/ms/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,28 @@
+package com.ms.msscbeerservice.web.controller;
+
+import com.ms.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId")UUID beerId){
+        return new ResponseEntity<BeerDto>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beerDto){
+        return  new ResponseEntity<BeerDto>(BeerDto.builder().build(), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public  ResponseEntity updateBeerById(@PathVariable("beerId")UUID beerId, @RequestBody BeerDto beerDto){
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
