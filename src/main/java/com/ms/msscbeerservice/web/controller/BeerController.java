e93b560dec98ede34cdc6b336d1be9f1a12c94e4
manueldvr <manueldvr@yahoo.com>
2023-05-17 10:53:21 -0300

    Controller validation. Exception handler.

--- src/main/java/com/ms/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/ms/msscbeerservice/web/controller/BeerController.java
@@ -3,6 +3,7 @@ package com.ms.msscbeerservice.web.controller;
 import com.ms.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -17,12 +18,12 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beerDto){
+    public ResponseEntity<BeerDto> saveNewBeer(@Validated @RequestBody BeerDto beerDto){
         return  new ResponseEntity<BeerDto>(BeerDto.builder().build(), HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public  ResponseEntity updateBeerById(@PathVariable("beerId")UUID beerId, @RequestBody BeerDto beerDto){
+    public  ResponseEntity updateBeerById(@PathVariable("beerId")UUID beerId, @Validated @RequestBody BeerDto beerDto){
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
