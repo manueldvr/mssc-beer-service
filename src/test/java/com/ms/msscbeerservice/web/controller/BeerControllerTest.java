41c6f5d48ed8e49f45c09a1611bef960da16426a
manueldvr <manueldvr@yahoo.com>
2023-05-20 09:42:37 -0300

    Tests corrections because mvn mapstruct and mappings.

--- src/test/java/com/ms/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/ms/msscbeerservice/web/controller/BeerControllerTest.java
@@ -3,12 +3,14 @@ package com.ms.msscbeerservice.web.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ms.msscbeerservice.web.model.BeerDto;
+import com.ms.msscbeerservice.web.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -24,8 +26,14 @@ class BeerControllerTest {
     ObjectMapper objectMapper;
 
 
-    private String getJsonBeer() throws JsonProcessingException {
-        BeerDto beerDto = BeerDto.builder().build();
+    private String getValidJsonBeerDto() throws JsonProcessingException {
+        BeerDto beerDto = BeerDto.builder()
+                .beerName("Imperial")
+                .upc(9L)
+                .beerStyle(BeerStyle.LAGER)
+                .price(new BigDecimal(3))
+                .quantityOnHand(3)
+                .build();
         return objectMapper.writeValueAsString(beerDto);
     }
 
@@ -36,13 +44,13 @@ class BeerControllerTest {
 
     @Test
     void saveNewBeer() throws Exception {
-        String jsonBeer = getJsonBeer();
+        String jsonBeer = getValidJsonBeerDto();
         mockMvc.perform(post("/api/v1/beer").contentType(MediaType.APPLICATION_JSON).content(jsonBeer)).andExpect(status().isCreated());
     }
 
     @Test
     void updateBeerById() throws Exception {
-        String jsonBeer = getJsonBeer();
+        String jsonBeer = getValidJsonBeerDto();
         mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID()).contentType(MediaType.APPLICATION_JSON).content(jsonBeer)).andExpect(status().isNoContent());
     }
 }
